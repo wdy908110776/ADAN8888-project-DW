540e034c9a40f1a45bf3b9506ab69ab9ed448823
Dingyi Wang <wdy908110776@gmail.com>
2024-01-28 13:28:45 -0500

    cookiecutter inital setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Data Science Project that will be used to predict a high-threating crime occurrence in Boston area',
+    author='Dingyi Wang',
+    license='MIT',
+)
